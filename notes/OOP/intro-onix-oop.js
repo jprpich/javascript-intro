38c9d9239a6c6a0591bbbdd4e5712ebcb9b7c911
juan267 <j.gomez267@gmail.com>
2017-11-17 20:42:00 -0500

    more notes

--- notes/OOP/intro-onix-oop.js
+++ notes/OOP/intro-onix-oop.js
@@ -1,9 +1,5 @@
-
-
 // prototype
 // prototype chain
-
-
 var Cow = function(name) {
   this.stomages = 4
   this.muu = function() {
@@ -34,9 +30,14 @@ Dog.prototype.woof = function() {
 
 
 ozu = new Dog('ozu', 'bulldog')
+
+
 // Las 4 formas en las que this toma su valor
 
 // 1. El llamado Default: this es window
 // 2. Cuando se llama una funcion con new: this es {}
 // 3. Es cuando se llama una funcion como un metodo: this es el objecto sobre el que se invoco
 // 4. Cuando se llama una funcion usando .call() y .apply(): this es el primer argumetno que le pase a call y apply
+
+
+// Ejemplo de call y apply
